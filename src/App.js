bf6f54c99aae05376853bee11608330095daf1b3
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2021-12-30 14:36:58 +0700

    Make Button Bootstrap React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
@@ -7,17 +9,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Afdi Fauzul Bahar Learning <code>React js</code> From youtube to Hero.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://github.com/afdi11"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Github
         </a>
       </header>
+      <h1>E-Commerce Dashboard</h1>
+      <button>Simple HTML Button</button>
+      <Button>Bootstrap Button</Button>
     </div>
   );
 }
