dd53c1c8444dbec9a3f2472f8545e6427c008e91
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2021-12-31 09:59:24 +0700

    Update AddProduct.js

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -2,7 +2,7 @@ import React from 'react';
 function AddProduct(){
     return(
         <div>
-            <h1>Halaman AddProduct</h1>
+            <h1>Halaman Add Product</h1>
         </div>
     )
 }
