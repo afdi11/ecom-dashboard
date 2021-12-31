255b2876fdf730594133fefb65fab4cc663cc8b4
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2021-12-31 08:50:37 +0700

    Membuat Halaman Login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap';
 import React from 'react';
-
+ 
 function App() {
   return (
     <div className="App">
