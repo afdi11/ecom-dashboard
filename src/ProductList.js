a464a0a7d7b0ad64589315095e6e824a52e53aad
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2022-02-02 11:26:01 +0700

    Retrieve All Data Product

--- src/ProductList.js
+++ src/ProductList.js
@@ -4,42 +4,55 @@ import { Table } from "react-bootstrap";
 import { baseurl } from "./services/api";
 
 function ProductList(){
-    const [data,setData]=useState("");
-    baseurl.get('listProduct')
-        .then(function(response){
+    const [data, setData] = useState([]);
+    useEffect(async() => {
+        try {
+            const response = await baseurl.get('listProduct');
             console.log(response.data);
-            setData(response.data);
-        }).catch(function (error){
-            console.log(error);
-        })
+            setData(response.data)
+            } catch (error) {
+            console.error(error);
+            }
+    }, []);
+    console.log(data)
     return(
         <div>
             <Header/>
             <h1>Product Lists</h1>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                    <th></th>
-                    <th>Destinasi</th>
-                    <th>Harga</th>
-                    <th>Deskripsi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            nomor
-                        </td>
-                        <td>nama</td>
-                        <td>
-                            Harga
-                        </td>
-                        <td>
-                            Deskripsi
-                        </td>
-                    </tr>
-                </tbody>
+            <div className="col-sm-8 offset-sm-2">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>No.</th>
+                        <th>Photo</th>
+                        <th>Destinasi</th>
+                        <th>Harga</th>
+                        <th>Deskripsi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {data.map((item)=>(
+                        <tr>
+                            <td>
+                                {item.ID}
+                            </td>
+                            <td>
+                                <img style={{width:100}}src={"http://localhost:8000/"+item.File_path}></img>
+                            </td>
+                            <td>
+                                {item.Nama}
+                            </td>
+                            <td>
+                                {item.Harga}
+                            </td>
+                            <td>
+                                {item.Deskripsi}
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
                 </Table>
+            </div>
         </div>
     )
 }
