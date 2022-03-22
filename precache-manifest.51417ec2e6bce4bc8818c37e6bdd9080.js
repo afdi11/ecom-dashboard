9ad34addc40f48a3dce26caabc822d6165d3f3a2
Afdi Fauzul Bahar <45322055+afdi11@users.noreply.github.com>
2022-03-22 09:23:34 +0700

    Updates

---
+++ precache-manifest.51417ec2e6bce4bc8818c37e6bdd9080.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b256ddada81b3b92ada1",
+    "url": "/ecom-dashboard/static/css/main.1f3a25d6.chunk.css"
+  },
+  {
+    "revision": "b256ddada81b3b92ada1",
+    "url": "/ecom-dashboard/static/js/main.b256ddad.chunk.js"
+  },
+  {
+    "revision": "8c94f7614dbddbd4c411",
+    "url": "/ecom-dashboard/static/js/1.8c94f761.chunk.js"
+  },
+  {
+    "revision": "d3b3398aa9f886273405",
+    "url": "/ecom-dashboard/static/css/2.54fe5dbb.chunk.css"
+  },
+  {
+    "revision": "d3b3398aa9f886273405",
+    "url": "/ecom-dashboard/static/js/2.d3b3398a.chunk.js"
+  },
+  {
+    "revision": "47f2bc6d00c8bd27a1ab",
+    "url": "/ecom-dashboard/static/js/runtime~main.47f2bc6d.js"
+  },
+  {
+    "revision": "fa67ec3c2de85af6fe431b27fdccb889",
+    "url": "/ecom-dashboard/index.html"
+  }
+];
